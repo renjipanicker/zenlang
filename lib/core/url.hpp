4107674518983b82a14a419a4088e7cd73d0f2d1
Renji Panicker <renji@segito.com>
2012-01-23 17:38:20 +0530

    Minor changes

--- lib/core/url.hpp
+++ lib/core/url.hpp
@@ -3,38 +3,7 @@
 namespace z {
     struct url {
     public:
-        inline void parse(const z::string& urlstr) {
-            _fullUrl = urlstr;
-
-            const std::string url_s = urlstr.val();
-            std::string protocol;
-            std::string host;
-            std::string path;
-            std::string query;
-
-            const std::string prot_end("://");
-            std::string::const_iterator prot_i = std::search(url_s.begin(), url_s.end(), prot_end.begin(), prot_end.end());
-            protocol.reserve(std::distance(url_s.begin(), prot_i));
-            std::transform(url_s.begin(), prot_i, std::back_inserter(protocol), std::ptr_fun<int,int>(tolower)); // protocol is icase
-            if( prot_i == url_s.end() )
-                return;
-
-            std::advance(prot_i, prot_end.length());
-            z::string::const_iterator path_i = std::find(prot_i, url_s.end(), '/');
-            host.reserve(distance(prot_i, path_i));
-            std::transform(prot_i, path_i, std::back_inserter(host), std::ptr_fun<int,int>(tolower)); // host is icase
-            z::string::const_iterator query_i = std::find(path_i, url_s.end(), '?');
-            path.assign(path_i, query_i);
-            if( query_i != url_s.end() )
-                ++query_i;
-
-            query.assign(query_i, url_s.end());
-
-            _protocol = protocol;
-            _host = host;
-            _path = path;
-            _query = query;
-        }
+        void parse(const z::string& urlstr);
 
         inline void dump() {
             std::cout << "url::dump(): fullPath=" << _fullUrl << ", protocol=" << _protocol << ", host=" << _host << ", path=" << _path << ", query=" << _query << std::endl;
