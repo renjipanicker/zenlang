e2665ad6afec2024247fc4c45f63b7770fcb322e
Renji Panicker <renji@segito.com>
2012-01-24 13:27:32 +0530

    Added File module

---
+++ lib/core/FileImpl.cpp
@@ -0,0 +1,16 @@
+#include "base/pch.hpp"
+#include "base/zenlang.hpp"
+#include "File.hpp"
+
+z::string File::completeBasePath(const z::string& path) {
+    z::string cbase = path;
+    z::string::size_type spos = cbase.rfind('/');
+    if(spos != z::string::npos) {
+        cbase = cbase.substr(spos+1);
+    }
+    z::string::size_type dpos = cbase.rfind('.');
+    if(dpos != z::string::npos) {
+        cbase = cbase.substr(0, dpos);
+    }
+    return cbase;
+}
